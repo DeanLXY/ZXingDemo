78fc143ced540f462c1e0fb00970072644323a92
wangxujie <wangxj0714@163.com>
2015-09-23 23:15:37 +0800

    Initial commit

---
+++ app/src/main/java/com/zijunlin/Zxing/Demo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.zijunlin.Zxing.Demo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String intent=getIntent().getStringExtra("code");
+        TextView txt=(TextView)findViewById(R.id.txtcode);
+        txt.setText(intent);
+        
+    }
+  
+  
+}
